99a3fa0a0ec76b7a812e77cd5985010a834c3481
Radekminecraft <radomirvondra@gmail.com>
2023-09-23 19:12:42 +0200

    rename

--- v_1_12_R1/src/main/java/tech/codemein/v_1_12_R1.java
+++ v_1_12_R1/src/main/java/tech/codemein/v_1_12_R1.java
@@ -1,12 +1,12 @@
 package tech.codemein;
 
-import net.minecraft.server.v1_14_R1.IChatBaseComponent;
-import net.minecraft.server.v1_14_R1.PacketPlayOutChat;
-import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import tech.codemein.actionbarapi.Actionbar;
 
-public class v_1_14 implements Actionbar {
+public class v_1_12_R1 implements Actionbar {
     @Override
     public void sendActionbar(Player p, String message) {
         IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}");
